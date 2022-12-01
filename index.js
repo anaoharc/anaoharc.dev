20ef40f51e0b3c518b5500fcbee0a255444e8d3b
Ana O <97709842+anaoharc@users.noreply.github.com>
2022-11-30 22:54:03 -0500

    Add files via upload
    source files for personal site

---
+++ index.js
@@ -0,0 +1,42 @@
+function hoverAnimated(el) {
+    const width = el.offsetWidth;
+    const position = el.offsetLeft;
+    insertAnimations(width, position);
+  }
+  
+  function outside() {
+  
+    if (document.querySelector('.active')) {
+      const active = document.querySelector('.active');
+      const width = active.offsetWidth;
+      const position = active.offsetLeft;
+      insertAnimations(width, position);
+      return;
+    }
+  
+    const animations = document.querySelector('.animations');
+    animations.style.left = '0px';
+    animations.style.width = '60px';
+  }
+  
+  function activeClass(el) {
+    const width = el.offsetWidth;
+    const position = el.offsetLeft;
+  
+    const removeActive = document.querySelectorAll('nav a');
+  
+    removeActive.forEach(element => {
+      element.classList.remove('active');
+    });
+  
+    el.classList.add('active');
+  
+    insertAnimations(width, position);
+  
+  }
+  
+  function insertAnimations(width, position) {
+    const animations = document.querySelector('.animations');
+    animations.style.left = position + 'px';
+    animations.style.width = width + 'px';
+  }
